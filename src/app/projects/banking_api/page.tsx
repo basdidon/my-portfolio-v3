5a6e0a079e2e4a3f53be3a9f8d6873e22121e11a
basdidon <39435008+basdidon@users.noreply.github.com>
2025-05-10 18:37:09 +0700

    -

--- src/app/projects/banking_api/page.tsx
+++ src/app/projects/banking_api/page.tsx
@@ -532,17 +532,117 @@ const BankingApiPage = () => {
                     &emsp;The backend is powered by Event Sourcing, implemented with MartenDb,
                     providing full traceability and consistency through immutable event logs.
                 </p>
-                <h2 className="text-2xl mt-8 mb-2 font-bold">Event Projections :</h2>
+                {/* Bank Account */}
+                <h2 className="text-2xl mt-8 mb-2 font-bold">BankAccount</h2>
                 <div className="md:px-2">
-                    <h3 className="text-xl mt-4 mb-2 font-bold">Bank Account Projection</h3>
+                    <h3 className="text-xl mt-4 mb-2 font-bold px-4">BankAccount.cs</h3>
+                    <div className="mt-2 md:px-4">
+                        <PrismGist
+                            codeLang="CSharp"
+                            gistRawFileUrl="https://gist.githubusercontent.com/basdidon/69f8c2f00ff92a9a19f2892941db6174/raw/5a82b8b2e360fa76c08534317f25ab1a2069c8fb/BankAccount.cs"
+                        />
+                    </div>
+                    <p>
+                        &emsp;This file defines the read model for a bank account. The BankAccount
+                        class represents the current state of a user's bank account. Key properties
+                        include:
+                    </p>
+                    <ul className=" list-inside list-disc mt-2">
+                        <li>
+                            <span className=" font-bold text-yellow-300">Id : </span>Unique
+                            identifier for the account (likely assigned by the event stream).
+                        </li>
+                        <li>
+                            <span className=" font-bold text-yellow-300">OwnerId : </span>The user
+                            who owns the account.
+                        </li>
+                        <li>
+                            <span className=" font-bold text-yellow-300">AccountNumber : </span>
+                            Human-readable identifier for the account (e.g., "123-456789-0").
+                        </li>
+                        <li>
+                            <span className=" font-bold text-yellow-300">Balance : </span>
+                            Current balance of the account.
+                        </li>
+                        <li>
+                            <span className=" font-bold text-yellow-300">IsFrozen : </span>
+                            Boolean flag indicating whether the account is frozen.
+                        </li>
+                    </ul>
+                    <h3 className="text-xl mt-4 mb-2 font-bold px-4">Events</h3>
+                    <p>
+                        These immutable event records represent actions that occur over time. They
+                        include:
+                    </p>
+                    <ul className=" list-inside list-disc my-2 space-y-2">
+                        <li>
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                AccountCreated
+                            </span>
+                            : Fired when an account is opened.
+                        </li>
+                        <li>
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                MoneyDeposit
+                            </span>
+                            ,
+                            <span className="bg-zinc-700 py-1 px-2 rrounded-md m-1">
+                                MoneyWithdraw
+                            </span>
+                            : For deposit and withdrawal operations.
+                        </li>
+                        <li>
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">MoneySent</span>,
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                MoneyRecieved
+                            </span>
+                            : For transferring money between accounts.
+                        </li>
+                        <li>
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                AccountFrozen
+                            </span>
+                            ,
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                AccountUnfrozen
+                            </span>
+                            : Change the account's status.
+                        </li>
+                        <li>
+                            <span className="bg-zinc-700 py-1 px-2 rounded-md m-1">
+                                AccountClosed
+                            </span>
+                            : Indicates that the account has been terminated.
+                        </li>
+                    </ul>
+                    <div className="md:px-4">
+                        <PrismGist
+                            codeLang="CSharp"
+                            gistRawFileUrl="https://gist.githubusercontent.com/basdidon/69f8c2f00ff92a9a19f2892941db6174/raw/5a82b8b2e360fa76c08534317f25ab1a2069c8fb/events.cs"
+                        />
+                    </div>
+                    <h3 className="text-xl mt-4 mb-2 font-bold px-4">BankAccountProjection.cs</h3>
                     <div className="md:px-4">
                         <PrismGist
                             codeLang="CSharp"
                             gistRawFileUrl="https://gist.githubusercontent.com/basdidon/69f8c2f00ff92a9a19f2892941db6174/raw/e63f8ba016410fc9adcb69a7e28aef216a346a11/BankAccountProjection.cs"
                         />
                     </div>
+                    <p>
+                        This class maps events to changes in the BankAccount state using Marten's
+                        SingleStreamProjection&lt;T&gt;
+                    </p>
                 </div>
-
+                {/* Withdrawal */}
+                <h2 className="text-2xl mt-8 mb-2 font-bold">Withdrawal</h2>
+                <p>
+                    &emsp;To prevent a Teller or Admin from arbitrarily withdrawing funds from an
+                    account owned by a regular user, additional logic is required. When a user
+                    intends to make a withdrawal, they must inform the Teller, who then sends a
+                    withdrawal request to the server. The server generates and sends a One-Time
+                    Password (OTP) to the user. The user provides the OTP to the Teller, who then
+                    submits a withdrawal confirmation request to complete the process.
+                </p>
                 <h2 className="text-lg md:text-2xl mt-8 mb-2 font-bold">Endpoints :</h2>
                 <div className="md:px-4 overflow-x-auto scrollbar-hide w-full">
                     <table className="table-auto w-full text-left text-sm md:text-base px-4 border-collapse border-spacing-x-4 bg-zinc-600 rounded-xl overflow-hidden">
--- src/components/PrismGist.tsx
+++ src/components/PrismGist.tsx
@@ -6,6 +6,7 @@ import "prismjs/components/prism-csharp";
 import "prismjs/components/prism-javascript"; // load specific language
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import Link from "next/link";
 
 const LanguageClassnameMap = {
     CSharp: "language-csharp",
@@ -22,18 +23,43 @@ type Props = {
 export default function PrismGist({ codeLang, gistRawFileUrl, fontSize = 12, classname }: Props) {
     const languageClassname = LanguageClassnameMap[codeLang];
     const [code, setCode] = useState("");
+    const [isSuccess, SetIsSuccess] = useState(false);
 
     useEffect(() => {
-        fetch(gistRawFileUrl)
-            .then((res) => res.text())
-            .then((data) => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(gistRawFileUrl);
+                if (!response.ok) throw new Error("Network response was not ok");
+                const data = await response.text();
                 setCode(data);
-            });
+                SetIsSuccess(true);
+            } catch {}
+        };
+
+        fetchData();
     }, [gistRawFileUrl]);
 
     useEffect(() => {
         Prism.highlightAll();
     }, [code]);
+
+    if (!isSuccess) {
+        return (
+            <div className="w-full bg-neutral-400  pt-2 pb-4 px-2 md:p-8">
+                <p className="text-lg md:text-2xl font-bold ">Failed to fetch :&#40;</p>
+                <p className="text-sm md:text-base block truncate mb-2">
+                    GistUrl : {gistRawFileUrl}
+                </p>
+                <Link
+                    href={gistRawFileUrl}
+                    target="_blank"
+                    className=" border-2 border-white text-white  font-bold py-1 px-4 rounded hover:bottom-0 hover:text-neutral-400 hover:bg-white"
+                >
+                    Open
+                </Link>
+            </div>
+        );
+    }
 
     return (
         <div className={classname}>
